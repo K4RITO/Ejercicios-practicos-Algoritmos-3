f70f3f591784c07c6f2378844e8ee73af893e884
K4RITO <matias.baragao@yahoo.com>
2026-05-19 10:33:32 -0300

    ejercicio 3 y 4 d repaso

---
+++ ejercicios repaso/ejercicio_3.py
@@ -0,0 +1,18 @@
+
+numero = int(input("Ingrese un número: "))
+
+if numero < 2:
+    print(f"{numero} no es un número primo.")
+else:
+    es_primo = True
+
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0:
+            es_primo = False
+            break
+
+    if es_primo:
+        print(f"{numero} es un número primo.")
+    else:
+        print(f"{numero} no es un número primo.")
+
